a70a08359d553ed61f14f4b0562f16e23fd4f165
fahmiin <fahmiinabdullah96@gmail.com>
2022-10-15 15:24:22 +0800

    feat: added balance update logic

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,3 +1,13 @@
 class Transaction < ApplicationRecord
   belongs_to :account
+
+  after_save :recalculate_balance
+  after_destroy :recalculate_balance
+
+  def recalculate_balance
+    return unless account.present?
+
+    account.balance = account.transactions.sum(:amount)
+    account.save!
+  end
 end
