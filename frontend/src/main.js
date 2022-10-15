63101942f8714a496a67caa68f309ae94f1837b5
fahmiin <fahmiinabdullah96@gmail.com>
2022-10-16 02:53:03 +0800

    chore: installed vuetify

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,10 +1,14 @@
+import 'core-js/stable'
+import 'regenerator-runtime/runtime'
 import Vue from 'vue'
 import App from './App.vue'
 import { createProvider } from './vue-apollo'
+import vuetify from './plugins/vuetify'
 
 Vue.config.productionTip = false
 
 new Vue({
   apolloProvider: createProvider(),
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
